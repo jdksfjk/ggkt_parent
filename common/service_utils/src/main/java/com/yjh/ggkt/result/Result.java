23eceb1f692bb61ceb617bf36d23b75b7986eaad
yjh <1093866975@qq.com>
2023-02-14 22:04:58 +0800

    service_vod项目的创建，讲师查询功能，讲师删除功能，统一返回结果，swagger配置

---
+++ common/service_utils/src/main/java/com/yjh/ggkt/result/Result.java
@@ -0,0 +1,69 @@
+package com.yjh.ggkt.result;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+@Data
+@ApiModel(value = "全局统一返回结果")
+public class Result<T> {
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public Result(){}
+
+    public static <T> Result<T> build(T body, Integer code, String message) {
+        Result<T> result = new Result<T>();
+        if (body != null) {
+            result.setData(body);
+        }
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+
+    /**
+     * 操作成功
+     * @param data  baseCategory1List
+     * @param <T>
+     * @return
+     */
+    public static<T> Result<T> ok(T data){
+        return build(data,200,"成功");
+    }
+
+    public static<T> Result<T> ok(){
+        return Result.ok(null);
+    }
+
+    /**
+     * 操作失败
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static<T> Result<T> fail(T data){
+        return build(data, 201,"失败");
+    }
+
+    public static<T> Result<T> fail(){
+        return Result.fail(null);
+    }
+
+    public Result<T> message(String msg){
+        this.setMessage(msg);
+        return this;
+    }
+
+    public Result<T> code(Integer code){
+        this.setCode(code);
+        return this;
+    }
+
+
+
+
+}
---
+++ service/service_vod/src/main/java/com/yjh/ggkt/vod/config/VodConfig.java
@@ -0,0 +1,9 @@
+package com.yjh.ggkt.vod.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.yjh.ggkt.vod.mapper")
+public class VodConfig {
+}
